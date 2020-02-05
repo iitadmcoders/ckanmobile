9f2e2d32cbf424f40a5d2d07823f85dca54256a3
Phanuel <ayukaphanuel@gmail.com>
2020-02-05 11:03:21 +0300

    android name chnages, ic launcher and the splash screen

--- android/app/src/main/java/org/iita/ckanmobile/MainActivity.java
+++ android/app/src/main/java/org/iita/ckanmobile/MainActivity.java
@@ -1,13 +1,32 @@
 package org.iita.ckanmobile;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(0x00000000); //transparent status bar
+            GeneratedPluginRegistrant.registerWith(this);
+            ViewTreeObserver viewTreeObserver = getFlutterView().getViewTreeObserver();
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                }
+            });
+        } else {
+            GeneratedPluginRegistrant.registerWith(this);
+        }
+
+    }
 }
